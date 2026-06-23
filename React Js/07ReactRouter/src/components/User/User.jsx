133c1f82e7a0d89a6925c340a96a4589a12feb2e
Ganpat Kumar <ganpatstudy@gmail.com>
2026-06-23 07:54:54 +0530

    react router added

---
+++ React Js/07ReactRouter/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+function User(){
+
+    const {id} = useParams()
+    return (
+        <div>
+            user: {id}
+        </div>
+    )
+}
+
+export default User
--- React Js/07ReactRouter/src/main.jsx
+++ React Js/07ReactRouter/src/main.jsx
@@ -1,10 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/ContactUs'
+import User from './components/User/User'
 
+
+//route done by nested array
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+        
+//       }, 
+//       {
+//         path : "about",
+//         element: <About />
+//       }, 
+//       {
+//         path : "contactUs",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+// Route done by withour array, by using the createBrowserRoute
+const router = createBrowserRouter(
+  createRoutesFromElements (
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contactUs' element={<Contact/>}/>
+      <Route path='user/:id' element={<User/>}/>
+
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
